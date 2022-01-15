90cbe4dc91d59b9f4ee7271a2e018f0e3556ea8b
ogenio <91586850+ogenio@users.noreply.github.com>
2022-01-14 23:42:54 -0300

    attgerador.sh

--- GENERADOR-VPS-MX/attgerador.sh
+++ GENERADOR-VPS-MX/attgerador.sh
@@ -67,7 +67,7 @@ echo -ne "\033[1;33mDescargando archivo: \033[1;31m[$arqx] "
 wget -O $HOME/$arqx ${IP}:81/${REQUEST}/${arqx} > /dev/null 2>&1 && echo -e "\033[1;31m- \033[1;32mRecibido con exito!" || echo -e "\033[1;31m- \033[1;31mFalla (no recibido!)"
 [[ -e $HOME/$arqx ]] && veryfy_fun $arqx
 done
-[[ ! -e /usr/bin/trans ]] && wget -O /usr/bin/trans https://raw.githubusercontent.com/ogenio/PROYECTOS_DESCONTINUADOS-1/master/GENERADOR-VPS-MX/Install/trans &> /dev/null
+[[ ! -e /usr/bin/trans ]] && wget -O /usr/bin/trans https://raw.githubusercontent.com/ogenio/PROYECTOS_DESCONTINUADOS/master/GENERADOR-VPS-MX/Install/trans &> /dev/null
 [[ -e /bin/http-server.py ]] && mv -f /bin/http-server.py /bin/http-server.sh && chmod +x /bin/http-server.sh
 [[ $(dpkg --get-selections|grep -w "bc"|head -1) ]] || apt-get install bc -y &>/dev/null
 [[ $(dpkg --get-selections|grep -w "screen"|head -1) ]] || apt-get install screen -y &>/dev/null
